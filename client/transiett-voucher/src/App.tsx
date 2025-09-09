8da73fce95001219ee45b045cd819eaf9c06bbd7
dendenmuniz <dendenmuniz@gmail.com>
2025-09-09 19:12:11 +0200

    refactor(client): tidy App layout/routes for new pages

--- client/transiett-voucher/src/App.tsx
+++ client/transiett-voucher/src/App.tsx
@@ -6,6 +6,8 @@ import {
 } from "react-router-dom";
 import { CampaignsPage } from './pages/CampaignsPage'
 import { CampaignDetail } from "./pages/CampaignDetail";
+import { NavBar } from "./components/NavBar";
+import { VouchersPage } from "./pages/VouchersPage";
 
 
 
@@ -13,13 +15,17 @@ function App() {
  
   return (
     <BrowserRouter>
-      <div className="max-w-5xl mx-auto p-4">
+      <div className="max-w-6xl mx-auto p-4">
         <header className="flex items-center justify-between mb-6">
-          <h1 className="text-2xl font-bold"><Link to="/">Transiett Vouchers</Link></h1>
+          
+          <NavBar />
+          
         </header>
+        
         <Routes>
           <Route path="/" element={<CampaignsPage />} />
           <Route path="/campaigns/:id" element={<CampaignDetail />} />
+          <Route path="/vouchers" element= {<VouchersPage />} />
         </Routes>
       </div>
     </BrowserRouter>
